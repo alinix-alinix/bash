380b0194239b3da5d41010a2d57c645a222750b2
alinix-alinix <79696698+alinix-alinix@users.noreply.github.com>
2024-09-05 01:02:23 +0300

    Update lookup.sh

--- lookup_tool/lookup.sh
+++ lookup_tool/lookup.sh
@@ -8,17 +8,33 @@ OUTPUT_FILE="hosts_info.csv"
 SSH_USER="s.stepanov"
 
 # Заголовок таблицы
-echo "fqdn;crontab;ip_addresses;cpu_cores;ram_size_GB;disk_count;disk_sizes_gb" > "$OUTPUT_FILE"
+echo "fqdn;server_type;crontab;ip_addresses;cpu_cores;ram_size_GB;disk_count;disk_sizes_gb" > "$OUTPUT_FILE"
 
 # Функция для перевода байтов в гигабайты
 convert_to_gb() {
     echo "scale=2; $1 / 1024 / 1024 / 1024" | bc
 }
 
+# Функция для определения типа сервера (физический или виртуальный)
+get_server_type() {
+    fqdn=$1
+    server_type=$(ssh -o BatchMode=yes -o ConnectTimeout=5 "$SSH_USER@$fqdn" "sudo dmidecode -s system-manufacturer" 2>/dev/null)
+    if [[ $? -ne 0 || -z "$server_type" ]]; then
+        echo "N/A"
+    elif [[ "$server_type" =~ "VMware"|"QEMU"|"VirtualBox"|"Microsoft"|"KVM"|"Xen" ]]; then
+        echo "$server_type"
+    else
+        echo "Physical"
+    fi
+}
+
 # Функция для получения информации с удаленного хоста
 get_host_info() {
     fqdn=$1
     echo "Обработка хоста: $fqdn"
+
+    # Определяем тип сервера
+    server_type=$(get_server_type "$fqdn")
 
     # Получаем содержимое crontab
     crontab_content=$(ssh -o BatchMode=yes -o ConnectTimeout=5 "$SSH_USER@$fqdn" 'sudo crontab -l' 2>/dev/null)
@@ -50,7 +66,7 @@ get_host_info() {
         ram_size_gb=$(convert_to_gb "$ram_size_bytes")
     fi
 
-    # Получаем информацию о физических дисках типа sda, sdb и т.д.
+    # Получаем информацию о физических дисках типа sda, sdb и nvme
     disk_info=$(ssh -o BatchMode=yes -o ConnectTimeout=5 "$SSH_USER@$fqdn" "sudo lsblk -b -o SIZE,NAME,TYPE -d | grep -E 'sd|nvme'" 2>/dev/null)
     if [[ $? -ne 0 || -z "$disk_info" ]]; then
         disk_count="N/A"
@@ -61,13 +77,12 @@ get_host_info() {
     fi
 
     # Записываем информацию в файл
-    echo "$fqdn;$crontab_content;$ip_addresses;$cpu_cores;$ram_size_gb;$disk_count;$disk_sizes_gb" >> "$OUTPUT_FILE"
+    echo "$fqdn;$server_type;$crontab_content;$ip_addresses;$cpu_cores;$ram_size_gb;$disk_count;$disk_sizes_gb" >> "$OUTPUT_FILE"
 }
 
 # Чтение списка хостов и обработка каждого
-
-for host in $HOSTS_FILE; 
-do
-        get_host_info "$host"
+for host in $HOSTS_FILE; do
+    get_host_info "$host"
 done
+
 echo "Информация о хостах сохранена в $OUTPUT_FILE"
